cbca2c703410ea9f2eba14b5ae499814c5fac1db
Jonathan "Duke" Leto <jonathan@leto.net>
2010-11-01 16:54:10 -0700

    Add the skeleton of a FASTA loader

---
+++ lib/Bio/Chado/Loader/FASTA.pm
@@ -0,0 +1,34 @@
+package Bio::Chado::Loader::FASTA;
+use Moose;
+with 'MooseX::Runnable';
+with 'MooseX::Getopt';
+
+has schema => (
+    is  => 'rw',
+    isa => 'Bio::Chado::Schema',
+);
+
+has features => (
+    is      => 'rw',
+    isa     => 'HashRef[Str]',
+    traits  => [ 'Hash' ],
+    default => sub { { } },
+    handles => {
+        add_feature    => 'set',
+        count_features => 'count',
+    },
+);
+
+with 'Bio::Chado::Loader';
+
+use autodie qw(:all);
+use Data::Dumper;
+use 5.010;
+
+sub run {
+    my ($self, %args) = @_;
+
+    exit 0;
+}
+
+1;
--- t/00-load.t
+++ t/00-load.t
@@ -1,12 +1,13 @@
 #!perl
 
-use Test::More tests => 2;
+use Test::More tests => 3;
 
 use lib './lib';
 
 BEGIN {
     use_ok( 'Bio::Chado::Loader' );
     use_ok( 'Bio::Chado::Loader::GFF3' );
+    use_ok( 'Bio::Chado::Loader::FASTA' );
 }
 
 diag( "Testing Bio::Chado::Loader $Bio::Chado::Loader::VERSION, Perl $], $^X" );
