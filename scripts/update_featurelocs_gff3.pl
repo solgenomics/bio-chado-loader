65ee24a301770b41f3189b9a3cdf1697b0d66c01
Surya Saha <ss2489@cornell.edu>
2014-03-04 18:19:03 -0500

    Added scaffold

---
+++ scripts/update_featurelocs_gff3.pl
@@ -0,0 +1,111 @@
+#!/usr/bin/perl
+
+=head1 NAME
+
+validate_gff.pl
+
+=head1 SYNOPSIS
+
+update_featurelocs_gff.pl -o ["organism"] -s ["DSN string"] -u [DB user] -p [password] -g [GFF3 file]
+
+=head1 COMMAND-LINE OPTIONS
+
+ -g  GFF3 file for updating locations (required)
+ -s  Database connection string e.g. "dbi:Pg:dbname=ss_cxgn_uploadtest\;host=localhost\;port=5432" (required)
+ -u  Database user name (required)
+ -p  Database user password (required)
+ -o  Organism name in quotes e.g. 'Solanum lycopersicum' (required)
+ -d  Debug messages (0 or 1)
+ -h  Help
+
+=cut
+
+use strict;
+use warnings;
+
+use File::Slurp;
+use Try::Tiny;
+use Getopt::Std;
+use Bio::Chado::Schema;
+
+our ( $opt_g, $opt_s, $opt_u, $opt_p, $opt_o, $opt_d, $opt_h );
+getopts('g:s:u:p:o:d:h');
+if ($opt_h) {
+	help();
+	exit;
+}
+if ( !$opt_g || !$opt_s || !$opt_u || !$opt_p || !$opt_o ) {
+	print "\nGFF3 file, databse credentials and organism name are required. See help below\n\n\n";
+	help();
+}
+
+#prep input data
+my $organism = $opt_o; chomp $organism;
+my $dsn = $opt_s; chomp $dsn;
+my $user = $opt_u; chomp $user;
+my $pass = $opt_p; chomp $pass;
+my $gff_file = $opt_g;
+my $gff      = read_file($gff_file)
+  or die "Could not open GFF file: $gff_file\n";
+
+if ($opt_d) {
+	print STDERR "Params parsed..\n";
+}
+
+my $schema= Bio::Chado::Schema->connect($dsn, $user, $pass);
+if (!$schema) { die "No schema is avaiable! \n"; }
+
+if ($opt_d) {
+	$schema->storage->debug(1);# print SQL statements
+}
+
+
+
+
+
+
+#----------------------------------------------------------------------------
+
+sub help {
+	print STDERR <<EOF;
+  $0:
+
+    Description:
+
+     The new chromosome backbone has to be loaded into the database using the GMOD bulk loader before new
+     coordinates can be added. The source feature is required to be present before any new featurelocs
+     can be placed on it.
+     
+    NOTE:
+     Will break if no ID field in attributes in GFF record 
+
+    Usage:
+      update_featurelocs_gff.pl -o ["organism"] -s ["DSN string"] -u [DB user] -p [password] -g [GFF3 file]
+      
+    Flags:
+
+ -g  GFF3 file for updating locations (required)
+ -s  Database connection string e.g. "dbi:Pg:dbname=ss_cxgn_uploadtest\;host=localhost\;port=5432" (required)
+ -u  Database user name (required)
+ -p  Database user password (required)
+ -o  Organism name in quotes e.g. 'Solanum lycopersicum' (required)
+ -d  Debug messages (0 or 1)
+ -h  Help
+
+EOF
+	exit(1);
+}
+
+=head1 LICENSE
+
+  Same as Perl.
+
+=head1 AUTHORS
+
+  Surya Saha <suryasaha@cornell.edu , @SahaSurya>
+
+=cut
+
+__END__
+
+
